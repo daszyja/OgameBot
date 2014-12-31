f292ae9a0c982de3f07321a1893187f53b099d21
adamhammes <peragon13@gmail.com>
2014-12-31 02:14:07 +0000

    Started work on reading messages

--- test.py
+++ test.py
@@ -9,4 +9,29 @@ ogame = OGame(  's120-us.ogame.gameforge.com',
                 'ogadrepr7cHubra' )
                 
 
-print ogame.is_researching()
+payload = {
+        'displayCategory': '10',
+        'displayPage': 1,
+        'siteType': '101',
+        'ajax': '1'
+}
+
+res = ogame.session.post( ogame.get_url( 'messages' ), data=payload).content
+soup = BeautifulSoup(res)
+
+
+with open( 'data.html', 'w' ) as f:
+    for line in res:
+        f.write( line )
+
+
+parent = soup.find( 'tr', {'id': 'TR3228375'})
+url = parent.find( 'a', {'class': 'overlay'})['href']
+
+print(url)
+
+res = ogame.session.get(url).content
+
+
+
+
