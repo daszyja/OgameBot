7b3e1d6443317c08f5b522f319f47e1f9dd7793f
adamhammes <peragon13@gmail.com>
2015-01-08 20:20:59 +0000

    set_resources() finished, added stubs for other methods

--- ogame/espionage.py
+++ ogame/espionage.py
@@ -3,9 +3,32 @@ import re
 
 class Espionage(object):
     def __init__(self, body):
-        self.body = body
+        # stripping out periods makes parsing the numbers easier
+        self.body = body.replace('.', '')
         
         self.set_resources()
+        self.set_defense()
         
+        
+    def get_count(self, name):
+        regex = re.compile(name + '\s*(\d+)', re.IGNORECASE)
+        m = regex.search(self.body)
+        return int(m.group(1))
+    
     def set_resources(self):
-        resource_re = ':\s* '
+        self.resources = {}
+        
+        for material in ['metal', 'crystal', 'deuterium']:
+            self.resources[material] = self.get_count(material + ':')
+       
+    def set_buildings(self):
+        pass
+    
+    def set_defense(self):
+        pass
+    
+    def set_fleet(self):
+        pass
+    
+    def set_meta(self):
+        pass
