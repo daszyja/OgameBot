bcf47996da28cf201e07d22d8670a273d6662391
adamhammes <peragon13@gmail.com>
2015-01-08 04:59:36 +0000

    Testing message generator

--- test.py
+++ test.py
@@ -1,37 +1,14 @@
-from errors import BAD_UNIVERSE_NAME, BAD_DEFENSE_ID, NOT_LOGGED
-import constants
+from __future__ import absolute_import
+from ogame.constants import *
 from bs4 import BeautifulSoup
-from ogame import OGame
+from ogame.ogame import OGame
+from ogame.message import Message
 import re
+
 
 ogame = OGame(  's120-us.ogame.gameforge.com',
                 'thaunatos',
                 'ogadrepr7cHubra' )
-                
 
-payload = {
-        'displayCategory': '10',
-        'displayPage': 1,
-        'siteType': '101',
-        'ajax': '1'
-}
-
-res = ogame.session.post( ogame.get_url( 'messages' ), data=payload).content
-soup = BeautifulSoup(res)
-
-
-with open( 'data.html', 'w' ) as f:
-    for line in res:
-        f.write( line )
-
-
-parent = soup.find( 'tr', {'id': 'TR3228375'})
-url = parent.find( 'a', {'class': 'overlay'})['href']
-
-print(url)
-
-res = ogame.session.get(url).content
-
-
-
-
+for message in ogame.messages(2):
+    print(message.subject + ' ' + str(message.time))
