bde98f49f625ea90f335ba1ed41fb16839062bbc
adamhammes <peragon13@gmail.com>
2015-01-16 22:09:27 +0000

    Added meta info and cargo calculations

--- ogame/espionage.py
+++ ogame/espionage.py
@@ -8,6 +8,7 @@ class Espionage(object):
         
         self.set_resources()
         self.set_defense()
+        self.set_meta()
         
         
     def get_count(self, name):
@@ -17,9 +18,11 @@ class Espionage(object):
     
     def set_resources(self):
         self.resources = {}
+        self.total_resources = 0
         
         for material in ['metal', 'crystal', 'deuterium']:
             self.resources[material] = self.get_count(material + ':')
+            self.total_resources += self.resources[material]
        
     def set_buildings(self):
         pass
@@ -31,4 +34,20 @@ class Espionage(object):
         pass
     
     def set_meta(self):
-        pass
+        # target name, location, time?
+        name_regex = re.compile( r'Player: (.*)\)', re.IGNORECASE)
+        self.target_name = name_regex.search(self.body).group(1)
+        
+        coords_regex = re.compile( r'\[(\d+):(\d+):(\d+)\]', re.IGNORECASE)
+        match = coords_regex.search(self.body)
+        self.coords = {}
+        self.coords['galaxy'] = int(match.group(1))
+        self.coords['system'] = int(match.group(2))
+        self.coords['position'] = int(match.group(3))
+
+    def cargos_needed(self, large=False, percentage=0.5):
+        res_possible = self.total_resources * percentage
+        if large:
+            return int(round(res_possible/25000)) + 1
+        else:
+            return int(round(res_possible/5000)) + 1
